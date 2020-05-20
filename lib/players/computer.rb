9dcf3cf97fc41a0f3d2d35ae7b6e6fa747f9c568
Tom Banish <tombanish@gmail.com>
2020-05-20 18:18:35 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,43 +17,14 @@ module Players
     
     def move(board)
       move = ""
+      
+      WIN_COMBINATIONS.each do |combo|
+        if board.cells[combo[0]] == @token && board.cells[combo[1]] == board.cells[combo[0]] && board.cells[combo[2]] == " "
+          move = combo[2] + 1
+        end
+      end
 
-      if @token == "X"
-        WIN_COMBINATIONS.each do |combo|
-          if @token == "X" && board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" && board.cells[combo[2]] == " "
-            move = combo[2] + 1
-          elsif @token == "X" && board.cells[combo[0]] == "X" && board.cells[combo[2]] == "X" && board.cells[combo[1]] == " "
-            move = combo[1] + 1
-          elsif @token == "X" && board.cells[combo[1]] == "X" && board.cells[combo[2]] == "X" && board.cells[combo[0]] == " "
-            move = combo[0] + 1
-          elsif @token == "X" && board.cells[combo[0]] == "O" && board.cells[combo[1]] == "O" && board.cells[combo[2]] == " "
-            move = combo[2] + 1
-          elsif @token == "X" && board.cells[combo[0]] == "O" && board.cells[combo[2]] == "O" && board.cells[combo[1]] == " "
-            move = combo[1] + 1
-          elsif @token == "X" && board.cells[combo[1]] == "O" && board.cells[combo[2]] == "O" && board.cells[combo[0]] == " "
-            move = combo[0] + 1
-          else
-            move = board.cells.index(" ") + 1
-          end
-        end
-      elsif @token == "O"
-        WIN_COMBINATIONS.each do |combo|
-          if board.cells[combo[0]] == "O" && board.cells[combo[1]] == "O" && board.cells[combo[2]] == " "
-            move = combo[2] + 1
-          elsif board.cells[combo[0]] == "O" && board.cells[combo[2]] == "O" && board.cells[combo[1]] == " "
-            move = combo[1] + 1
-          elsif board.cells[combo[1]] == "O" && board.cells[combo[2]] == "O" && board.cells[combo[0]] == " "
-            move = combo[0] + 1
-          elsif board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" && board.cells[combo[2]] == " "
-            move = combo[2] + 1
-          elsif board.cells[combo[0]] == "X" && board.cells[combo[2]] == "X" && board.cells[combo[1]] == " "
-            move = combo[1] + 1
-          elsif board.cells[combo[1]] == "X" && board.cells[combo[2]] == "X" && board.cells[combo[0]] == " "
-            move = combo[0] + 1
-          else
-            move = board.cells.index(" ") + 1
-          end
-        end
+      
       end
       move.to_s
     end
