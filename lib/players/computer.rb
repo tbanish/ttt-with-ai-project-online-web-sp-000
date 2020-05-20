a98a97ea5794805d32ef76eb6294052c33c25cc0
Tom Banish <tombanish@gmail.com>
2020-05-20 20:38:38 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,6 +16,17 @@ module Players
   ]
 
     def move(board)
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[6] == " "
+        "7"
+      elsif board.cells[8] == " "
+        "9"
+      else
       any_open_space(board)
     end
     
