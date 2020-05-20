7005512113fba5c0761aa4725fe0458ed00205ce
Tom Banish <tombanish@gmail.com>
2020-05-20 20:17:37 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -23,6 +23,11 @@ module Players
       move = board.cells.index(" ") + 1
       move.to_s
     end
+    
+    # def check_wins(board)
+    #   WIN_COMBINATIONS.each do |combo|
+    #     if board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" && board.cells[combo]
+    # end
       
       
       
